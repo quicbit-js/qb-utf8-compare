7b8de476fda8568e07ea8ad905c0ad2e8b710b0d
mvoss9000 <mvoss9000@yahoo.com>
2017-11-12 21:17:12 -0500

    apply standard format

--- index.js
+++ index.js
@@ -15,20 +15,20 @@
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 function compare (src1, off1, lim1, src2, off2, lim2) {
-    lim1 > off1 && lim2 > off2 && off1 >= 0 && lim1 > 0 && off2 >= 0 && lim2 > 0 || err('bad range')
-    var len1 = lim1 - off1
-    var len2 = lim2 - off2
-    var n = len1 < len2 ? len1 : len2
-    var i = 0
-    while (i < n && src1[i + off1] === src2[i + off2]) { i++ }
-    if (i === n) {
-        return len1 === len2 ? 0 : (len1 > len2 ? 1 : -1)
-    } else {
-        return codepoint(src1, off1, lim1, i + off1) > codepoint(src2, off2, lim2, i + off2) ? 1 : -1
-    }
+  lim1 > off1 && lim2 > off2 && off1 >= 0 && lim1 > 0 && off2 >= 0 && lim2 > 0 || err('bad range')
+  var len1 = lim1 - off1
+  var len2 = lim2 - off2
+  var n = len1 < len2 ? len1 : len2
+  var i = 0
+  while (i < n && src1[i + off1] === src2[i + off2]) { i++ }
+  if (i === n) {
+    return len1 === len2 ? 0 : (len1 > len2 ? 1 : -1)
+  } else {
+    return codepoint(src1, off1, lim1, i + off1) > codepoint(src2, off2, lim2, i + off2) ? 1 : -1
+  }
 }
 
-function err (msg) { throw Error (msg) }
+function err (msg) { throw Error(msg) }
 
 // figure out the codepoint at the given index 'idx' of a UTF8 encoded character by
 //  1   finding the index of the starting byte
@@ -37,32 +37,30 @@ function err (msg) { throw Error (msg) }
 //
 //  if the character is not complete (falls outside of off/lim) throw error
 function codepoint (src, off, lim, idx) {
-    var i = idx
-    switch (src[i] & 0xC0) {
-        case 0x00:                              // 00xxxxxx     // ASCII 0 - 63
-        case 0x40:                              // 01xxxxxx     // ASCII 64-127
-            return src[i]
-        case 0x80:                              // 10xxxxxx     - trailing byte
-            // find starting byte
-            var min = i - 5                     // limit to largest possible unicode (6 bytes)
-            if (min < off) { min = off }
-            while (i > min && ((src[i] & 0xC0) === 0x80)) { i-- }
-            (src[i] & 0xC0) === 0xC0 || err('no UTF8 starting byte in range: ' + idx + '..' + i )
-            // fallthrough...
-        case 0xC0:                              // 11xxxxxx     - starting byte
-            var c = src[i]
-            for (var n = 2; (c << n) & 0x80; n++) {}    // sequential 1's gives is number of bytes: (e.g. 1110xxxx has 3)
-            i + n <= lim || err(n + ' indicated UTF8 bytes don\'t fit in range: ' + idx + '..' + lim)
-            var ret =  c & (0xFF >> (n+1))       // start with (8 - (n+1)) LSBs
-            for (var ni = 1; ni < n; ni++) {
-                ret = (ret << 6) | (0x3F & src[i + ni])
-            }
-            return ret
-    }
+  var i = idx
+  switch (src[i] & 0xC0) {
+    case 0x00:                              // 00xxxxxx     // ASCII 0 - 63
+    case 0x40:                              // 01xxxxxx     // ASCII 64-127
+      return src[i]
+    case 0x80:                              // 10xxxxxx     - trailing byte
+      // find starting byte
+      var min = i - 5                       // limit to largest possible unicode (6 bytes)
+      if (min < off) { min = off }
+      while (i > min && ((src[i] & 0xC0) === 0x80)) { i-- }
+      (src[i] & 0xC0) === 0xC0 || err('no UTF8 starting byte in range: ' + idx + '..' + i)
+      // fallthrough...
+    case 0xC0:                              // 11xxxxxx     - starting byte
+      var c = src[i]
+      for (var n = 2; (c << n) & 0x80; n++) {}    // sequential 1's gives is number of bytes: (e.g. 1110xxxx has 3)
+      i + n <= lim || err(n + ' indicated UTF8 bytes don\'t fit in range: ' + idx + '..' + lim)
+      var ret = c & (0xFF >> (n + 1))       // start with (8 - (n+1)) LSBs
+      for (var ni = 1; ni < n; ni++) {
+        ret = (ret << 6) | (0x3F & src[i + ni])
+      }
+      return ret
+  }
 }
 
 compare.codepoint = codepoint
 
 module.exports = compare
-
-
--- test.js
+++ test.js
@@ -19,102 +19,101 @@ var utf8 = require('qb-utf8-ez')
 var cmp = require('.')
 
 test('codepoint', function (t) {
-    t.table_assert([
-        [ 'src',                        'off',  'lim',  'idx',  'exp' ],
+  t.table_assert([
+        [ 'src', 'off', 'lim', 'idx', 'exp' ],
 
         // ASCII
-        [ ' ',                          0,      1,       0,      '0x20' ],
-        [ 'a',                          0,      1,       0,      '0x61' ],
-        [ 'abc',                        0,      3,       0,      '0x61' ],
-        [ 'abc',                        0,      3,       1,      '0x62' ],
-        [ 'abc',                        0,      3,       2,      '0x63' ],
+        [ ' ', 0, 1, 0, '0x20' ],
+        [ 'a', 0, 1, 0, '0x61' ],
+        [ 'abc', 0, 3, 0, '0x61' ],
+        [ 'abc', 0, 3, 1, '0x62' ],
+        [ 'abc', 0, 3, 2, '0x63' ],
 
         // 2-byte min value  (11000000 10000000)
-        [ [0xC0, 0x80],                 0,       2,      0,       '0x0' ],
-        [ [0xC0, 0x80],                 0,       2,      1,       '0x0' ],
+        [ [0xC0, 0x80], 0, 2, 0, '0x0' ],
+        [ [0xC0, 0x80], 0, 2, 1, '0x0' ],
         // 2 byte max value (11011111 10111111)
-        [ [0xDF, 0xBF],                 0,       2,      0,       '0x7FF' ],
-        [ [0xDF, 0xBF],                 0,       2,      1,       '0x7FF' ],
+        [ [0xDF, 0xBF], 0, 2, 0, '0x7FF' ],
+        [ [0xDF, 0xBF], 0, 2, 1, '0x7FF' ],
 
         // 3 byte min value  (11100000 10000000 10000000)
-        [ [0xE0, 0x80, 0x80],           0,       3,      0,       '0x0' ],
-        [ [0xE0, 0x80, 0x80],           0,       3,      1,       '0x0' ],
-        [ [0xE0, 0x80, 0x80],           0,       3,      2,       '0x0' ],
+        [ [0xE0, 0x80, 0x80], 0, 3, 0, '0x0' ],
+        [ [0xE0, 0x80, 0x80], 0, 3, 1, '0x0' ],
+        [ [0xE0, 0x80, 0x80], 0, 3, 2, '0x0' ],
         // 3 byte max value (11101111 10111111 10111111)
-        [ [0xEF, 0xBF, 0xBF],           0,       3,      0,       '0xFFFF' ],
-        [ [0xEF, 0xBF, 0xBF],           0,       3,      1,       '0xFFFF' ],
-        [ [0xEF, 0xBF, 0xBF],           0,       3,      2,       '0xFFFF' ],
+        [ [0xEF, 0xBF, 0xBF], 0, 3, 0, '0xFFFF' ],
+        [ [0xEF, 0xBF, 0xBF], 0, 3, 1, '0xFFFF' ],
+        [ [0xEF, 0xBF, 0xBF], 0, 3, 2, '0xFFFF' ],
 
         // 4 byte min value  (11110000 10000000 10000000 10000000)
-        [ [0xF0, 0x80, 0x80, 0x80],     0,       4,      0,       '0x0' ],
-        [ [0xF0, 0x80, 0x80, 0x80],     0,       4,      1,       '0x0' ],
-        [ [0xF0, 0x80, 0x80, 0x80],     0,       4,      2,       '0x0' ],
-        [ [0xF0, 0x80, 0x80, 0x80],     0,       4,      3,       '0x0' ],
+        [ [0xF0, 0x80, 0x80, 0x80], 0, 4, 0, '0x0' ],
+        [ [0xF0, 0x80, 0x80, 0x80], 0, 4, 1, '0x0' ],
+        [ [0xF0, 0x80, 0x80, 0x80], 0, 4, 2, '0x0' ],
+        [ [0xF0, 0x80, 0x80, 0x80], 0, 4, 3, '0x0' ],
         // 4 byte max value (11110111 10111111 10111111 10111111)
-        [ [0xF7, 0xBF, 0xBF, 0xBF],     0,       4,      0,       '0x1FFFFF' ],
-        [ [0xF7, 0xBF, 0xBF, 0xBF],     0,       4,      1,       '0x1FFFFF' ],
-        [ [0xF7, 0xBF, 0xBF, 0xBF],     0,       4,      2,       '0x1FFFFF' ],
-        [ [0xF7, 0xBF, 0xBF, 0xBF],     0,       4,      3,       '0x1FFFFF' ],
+        [ [0xF7, 0xBF, 0xBF, 0xBF], 0, 4, 0, '0x1FFFFF' ],
+        [ [0xF7, 0xBF, 0xBF, 0xBF], 0, 4, 1, '0x1FFFFF' ],
+        [ [0xF7, 0xBF, 0xBF, 0xBF], 0, 4, 2, '0x1FFFFF' ],
+        [ [0xF7, 0xBF, 0xBF, 0xBF], 0, 4, 3, '0x1FFFFF' ],
 
         // 3-byte
-        [ '吃飲',                0,       6,      0,       '0x5403' ],
-        [ '吃飲',                0,       6,      1,       '0x5403' ],
-        [ '吃飲',                0,       6,      2,       '0x5403' ],
-        [ '吃飲',                0,       6,      3,       '0x98F2' ],
-        [ '吃飲',                0,       6,      4,       '0x98F2' ],
-        [ '吃飲',                0,       6,      5,       '0x98F2' ],
+        [ '吃飲', 0, 6, 0, '0x5403' ],
+        [ '吃飲', 0, 6, 1, '0x5403' ],
+        [ '吃飲', 0, 6, 2, '0x5403' ],
+        [ '吃飲', 0, 6, 3, '0x98F2' ],
+        [ '吃飲', 0, 6, 4, '0x98F2' ],
+        [ '吃飲', 0, 6, 5, '0x98F2' ]
 
-    ], function (src, off, lim, idx) {
-        src = typeof src === 'string' ? utf8.buffer(src) : src
-        var cp = cmp.codepoint(src, off, lim, idx)
-        return '0x' + cp.toString(16).toUpperCase()
-    } )
+  ], function (src, off, lim, idx) {
+    src = typeof src === 'string' ? utf8.buffer(src) : src
+    var cp = cmp.codepoint(src, off, lim, idx)
+    return '0x' + cp.toString(16).toUpperCase()
+  })
 })
 
 test('codepoint errors', function (t) {
-    t.table_assert([
-        [ 'src',                        'off',  'lim',  'idx',      'exp' ],
-        [ [0xF7, 0xBF, 0xBF, 0xBF],     0,       3,      0,         /bytes don.t fit in range: 0..3/],
-        [ [0xF7, 0xBF, 0xBF, 0xBF],     1,       4,      1,         /no UTF8 starting byte in range: 1..1/],
-    ], function (src, off, lim, idx) {
-        src = typeof src === 'string' ? utf8.buffer(src) : src
-        var cp = cmp.codepoint(src, off, lim, idx)
-        return '0x' + cp.toString(16).toUpperCase()
-    }, { assert: 'throws' })
+  t.table_assert([
+        [ 'src', 'off', 'lim', 'idx', 'exp' ],
+        [ [0xF7, 0xBF, 0xBF, 0xBF], 0, 3, 0, /bytes don.t fit in range: 0..3/],
+        [ [0xF7, 0xBF, 0xBF, 0xBF], 1, 4, 1, /no UTF8 starting byte in range: 1..1/]
+  ], function (src, off, lim, idx) {
+    src = typeof src === 'string' ? utf8.buffer(src) : src
+    var cp = cmp.codepoint(src, off, lim, idx)
+    return '0x' + cp.toString(16).toUpperCase()
+  }, { assert: 'throws' })
 })
 
 test('compare', function (t) {
-    t.table_assert([
-        [ 'src1',       'off1', 'lim1', 'src2',     'off2', 'lim2', 'exp' ],
-        [ 'abcd',       0,      4,      'abcd',     0,      4,      0 ],
-        [ 'abcd',       1,      4,      'abcd',     0,      4,      1 ],
-        [ 'abcd',       0,      4,      'abcd',     1,      4,      -1 ],
+  t.table_assert([
+        [ 'src1', 'off1', 'lim1', 'src2', 'off2', 'lim2', 'exp' ],
+        [ 'abcd', 0, 4, 'abcd', 0, 4, 0 ],
+        [ 'abcd', 1, 4, 'abcd', 0, 4, 1 ],
+        [ 'abcd', 0, 4, 'abcd', 1, 4, -1 ],
 
         // all else equal, longer means greater
-        [ 'abcd',       0,      4,      'abcd',     0,      3,      1 ],
-        [ 'abcd',       0,      3,      'abcd',     0,      4,      -1 ],
-        [ 'abc',       0,      3,       'abc飲',     0,      6,      -1 ],
-        [ 'abc飲',      0,      6,      'abc',     0,      3,      1 ],
+        [ 'abcd', 0, 4, 'abcd', 0, 3, 1 ],
+        [ 'abcd', 0, 3, 'abcd', 0, 4, -1 ],
+        [ 'abc', 0, 3, 'abc飲', 0, 6, -1 ],
+        [ 'abc飲', 0, 6, 'abc', 0, 3, 1 ],
 
-        [ 'ab 吃',       0,      6,      'ab 飲',     0,      6,      -1 ],
-        [ 'ab 飲',       0,      6,      'ab 吃',     0,      6,      1 ],
+        [ 'ab 吃', 0, 6, 'ab 飲', 0, 6, -1 ],
+        [ 'ab 飲', 0, 6, 'ab 吃', 0, 6, 1 ],
 
-        [ '吃飲，好吃',   0,      15,      '吃飲，好吃',     0,      15,      0 ],
-        [ '吃飲，好吃!',  0,      16,      '吃飲，好吃!',    0,      15,      1 ],
-        [ '吃飲，好吃!',  0,      15,      '吃飲，好吃',     0,      16,      -1 ],
+        [ '吃飲，好吃', 0, 15, '吃飲，好吃', 0, 15, 0 ],
+        [ '吃飲，好吃!', 0, 16, '吃飲，好吃!', 0, 15, 1 ],
+        [ '吃飲，好吃!', 0, 15, '吃飲，好吃', 0, 16, -1 ]
 
-    ], function(src1, off1, lim1, src2, off2, lim2) {
-        return cmp(utf8.buffer(src1), off1, lim1, utf8.buffer(src2), off2, lim2)
-    } )
+  ], function (src1, off1, lim1, src2, off2, lim2) {
+    return cmp(utf8.buffer(src1), off1, lim1, utf8.buffer(src2), off2, lim2)
+  })
 })
 
-
 test('compare errors', function (t) {
-    t.table_assert([
-        [ 'src1',       'off1', 'lim1', 'src2',     'off2', 'lim2', 'exp' ],
-        [ 'abcd',       0,      0,      'abcd',     0,      4,      /bad range/ ],
+  t.table_assert([
+        [ 'src1', 'off1', 'lim1', 'src2', 'off2', 'lim2', 'exp' ],
+        [ 'abcd', 0, 0, 'abcd', 0, 4, /bad range/ ]
 
-    ], function(src1, off1, lim1, src2, off2, lim2) {
-        return cmp(utf8.buffer(src1), off1, lim1, utf8.buffer(src2), off2, lim2)
-    }, { assert: 'throws' })
+  ], function (src1, off1, lim1, src2, off2, lim2) {
+    return cmp(utf8.buffer(src1), off1, lim1, utf8.buffer(src2), off2, lim2)
+  }, { assert: 'throws' })
 })
